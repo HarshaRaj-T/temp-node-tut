d5d7c667694fd0053970a9cf1c0d89a5699c2fd6
Harsha Raj T <tharsharaj30@gmail.com>
2022-10-16 11:16:00 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOW !!
+// __dirname - path to current directory
+// __filename - file name
+// require - funtion to use modules (CommonJS)
+// modules - info about current modules(file)
+// process - info about environment where the program is being executed
+
+
+console.log(__dirname)
+
+setTimeout(()=>{
+    console.log('hello')
+},5)
